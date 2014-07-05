37b57128ff3cd5530002e35633b9d1934858175a
bornreddy <marisa.reddy@gmail.com>
2014-07-05 19:43:40 -0400

    added mongoose connect/disconnect

--- app.js
+++ app.js
@@ -1,62 +1,100 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var qr = require('./routes/qr');
+mongoose.connection.on("connected", function(ref) {
+    console.log("Connected to my database!");
+
+    var path = require('path');
+    var favicon = require('static-favicon');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
+    var debug = require('debug')('Osu');
+
+    var routes = require('./routes/index');
+    var users = require('./routes/users');
+    var qr = require('./routes/qr');
 
 
-var app = express();
+    var app = express();
+    
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+    app.use(favicon());
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use('/', routes);
+    app.post('/input_username',qr.show_qr);  
+    app.use('/users', users);
 
-app.use('/', routes);
-app.post('/input_username',qr.show_qr);  
-app.use('/users', users);
+    /// catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    /// error handlers
 
-/// error handlers
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+    // production error handler
+    // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: {}
         });
     });
-}
+    app.set('port', process.env.PORT || 3000);
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    var server = app.listen(app.get('port'), function() {
+      debug('Express server listening on port ' + server.address().port);
     });
+    module.exports = app;
 });
 
+//if error on mongoose connection start up
+mongoose.connection.on("error", function(err) {
+  console.error('Failed to connect to DB my database on startup ', err);
+});
 
-module.exports = app;
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection to DB : database disconnected');
+});
+
+//close the mongoose connection
+var gracefulExit = function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection with DB : database is disconnected through app termination');
+    process.exit(0);
+  });
+};
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
+
+try {
+  mongoose.connect('mongodb://localhost/test');
+  console.log("Trying to connect to DB");
+} catch (err) {
+  console.log("Sever initialization failed " , err.message);
+}
--- routes/qr.js
+++ routes/qr.js
@@ -1,6 +1,22 @@
 exports.show_qr = function(req,res) {
+	//get the username and show a qr from a generated hash run through str2qr. 
+	//add the qr hash to mongo under a list of "pending hash for approval"
+	//the strings should only be valid for two minutes -- mongod ttl 
+	//render the page with the username
 	username = req.body.input_username
 	console.log(username)
 	res.render('qr', { title: 'qr.jade' });
+	res.end()
 }
 
+var str2qr = function() {
+  //given a random hash string, generate the qr code and render to the screen	
+
+}
+
+exports.phone_info = function(req,res) {
+	//get the username
+	//check the input qr string against any pending strings in the database
+	//check the user's phone hash against the mongo db.
+	//if this is all good, render the login. 
+}
