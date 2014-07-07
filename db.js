76c9d3f663982c1ff74ce3678353853cb484100c
bornreddy <marisa.reddy@gmail.com>
2014-07-06 21:35:48 -0400

    Added in upsert user_hashes method in db.js. Signed-off-by: bornreddy <marisa.reddy@gmail.com>

---
+++ db.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+//create url Schema
+var userSchema = mongoose.Schema({
+	username: {type : String},
+	userhash: {type: String},
+	pending_hashes: { type : Array , "default" : [] }
+});
+
+userSchema.methods.show = function () {
+	var user_info = "username: " + this.username + 
+	"\nuser_hash: " + this.userhash 
+	"\npending_hashes" + this.pending_hashes;
+	console.log(user_info);
+};
+
+var Users = mongoose.model('Users', userSchema);
+
+//given username, push a validation hash onto their pending hashes array
+exports.add_hash= function(name,validation_hash, callback) {
+	var query = {"username": name};
+	var update = { $addToSet: { pending_hashes: validation_hash } }
+	var options = {upsert: true};
+	Users.findOneAndUpdate(query, update, options, function(err, user) {
+		if (err) {
+			console.log('got an error');
+		} else {
+			console.log(user)
+			callback(user)
+		}
+  });
+};
+
+
+//get userhash from username
+//get pending hashes from username
+//add a pending hash to a username
+//delete a pending hash after ttl
--- routes/qr.js
+++ routes/qr.js
@@ -1,23 +1,32 @@
-var crypto = require('crypto')
+var crypto = require('crypto');
+var db = require('../db')
+
+
 
 exports.show_qr = function(req,res) {
-	//get the username and generate a hash 
-	//add the qr hash to mongo under a list of "pending hash for approval"
-	//the strings should only be valid for two minutes -- mongod ttl
-	//send the qr hash to the client-side javascript to render it there. res.send()
-	username = req.body.input_username;
-	console.log(username);
-	res.render('qr', { title: 'qr.jade' });
+	//TODO: the strings should only be valid for two minutes -- mongod ttl
+	//TODO: send the qr hash to the client-side javascript to render it there. res.send()
+	var username = req.body.input_username;
 	var qr_hash = crypto.randomBytes(200).toString('hex');
-	console.log(qr_hash)
-	res.send(qr_hash)
-	res.end();
+    db.add_hash(username, qr_hash, function(res2) {
+    	console.log("after adding to pending_hash to username");
+    	res.render('qr', { title: 'qr.jade' });
+    	res.send(qr_hash);
+    	res.end();
+    });
 }
 
+
+// --get the username and generate hash
+// 	   --db : for a username, add hash to pending hashes
+// 	--they see hash, send back the hash
+// 	-- db : check if userhash and username go together.
+// 	-- db : given userhash, get pending images 
 
 exports.phone_info = function(req,res) {
 	//get the username
 	//check the input qr string against any pending strings in the database
 	//check the user's phone hash against the mongo db.
-	//if this is all good, render the login. 
+	//if this is all good, render the login.
+	console.log("in second function") 
 }
